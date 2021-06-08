6ed5fe0d8fcc7bf4906b7d62c698cc0fcfe42614
unknown <connordupuis760@gmail.com>
2021-06-08 12:36:21 -0400

    Orbiter with labels

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,28 +7,45 @@
         <b-nav-item href="/wittyskates.github.io/">About</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ms-auto">
-        <b-nav-item href="#">Resume</b-nav-item>
+        <b-nav-item href="#" style="padding-right:10px">Resume</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
-    <div id="circle-orbit-container">
-
-      <!-- Circles closest to the central point -->
-      <div id="inner-orbit">
-        <div class="inner-orbit-cirlces"></div>
+    
+    <div id="solarSketch">	
+      <div id="sol"><p id="sol-label">0</p></div>	
+      <div id="Planet1-orbit">
+        <div id="Planet1"></div>
+        <p id="Planet1-label">1</p>
       </div>
-
-      <!-- Circles second closest to the central point -->
-      <div id="middle-orbit">
-        <div class="middle-orbit-cirlces"></div>
+      <div id="Planet2-orbit">
+        <div id="Planet2"></div>
+        <p id="Planet2-label">2</p>
       </div>
-
-      <!-- Circles furthest away to the central point -->
-      <div id="outer-orbit">
-        <div class="outer-orbit-cirlces"></div>
-      </div>
-
+      <div id="Planet3-orbit">
+        <div id="Planet3-3-orbit">
+            <div id="Planet3-3"></div>
+          <p id="Planet3-3-label">3.3</p>
+        </div>
+        <div id="Planet3-1-orbit">
+            <div id="Planet3-1"></div>
+          <p id="Planet3-1-label">3.1</p>
+        </div>
+        <div id="Planet3-2-orbit">
+            <div id="Planet3-2"></div>
+          <p id="Planet3-2-label">3.2</p>
+        </div>
+        <div id="Planet3"></div>
+        <p id="Planet3-label">3</p>
+        </div>
+        <div id="Planet4-orbit">
+        <div id="Planet4-1-orbit">
+            <div id="Planet4-1"></div>
+          <p id="Planet4-1-label">4.1</p>
+        </div>
+        <div id="Planet4"></div>
+        <p id="Planet4-label">4</p>
+        </div>
     </div>
-
   </div>
 </template>
 
@@ -40,6 +57,7 @@ export default {
 
   }
 }
+
 </script>
 
 <style>
@@ -49,95 +67,534 @@ export default {
   background-color: #343a40;
 }
 
-/* ---------- Container for the orbiting circles animation ---------- */
-#circle-orbit-container {
-  position: relative;
-  top: 25px;
-  left: 25px;
-  height: 300px;
-  width: 300px;
+/* Basic Layout Bigger Orbiter*/
+
+body {
+    width: 100%;
+    height: 100%;
+    background-image: url(https://github.com/pesegal/CSS3-System-Sketch/blob/master/img/gg66203835.jpg?raw=true);
 }
 
-/* ---------- Inner orbit - This is the circles closest to the central point ---------- */
-#inner-orbit {
+#solarSketch {
+  position: relative;
+  top: 700px;
+  /* left: 50%; */
+}
+
+#container {
   position: absolute;
-  top: 75px;
-  left: 75px;
-  width: 150px;
-  height: 150px;
-  border: 2px #4A437F dashed;
-  border-radius: 100%;
+  top: 3%;
+  left: 2%;
+  border: 3px solid white;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+#container h1 {
+  color: white;
+  font-family: helvetica;
+  font-size: 20px;
+  
+}
+
+#container p {
+  margin-top: 0px;
+  color: white;
+  font-family: helvetica;
+  font-size: 15px;
+  font-style: italic;
+  align-content: center;
+}
+
+/*Sol Group*/
+
+#sol {
+  position: absolute;
+  top: 59%;
+  left: 50%;
+
+  background: white;
+  height: 100px;
+  width: 100px;
+  margin-top: -50px; 
+  margin-left: -50px;
+
+  border-color: white;
+  border-width: 10px;
+  border-style: solid;
+  border-radius: 50%;   
+}
+
+#sol-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -60px;
+  margin-top: -12px;
+  color: white;
+  font-size: 12px;
+}
+
+/* Planet 1 Group*/
+#Planet1 {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -10px;
+  margin-top: -10px;
+  background: white;
+  border-color: white;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 50%;
+}
+
+#Planet1-orbit {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  width: 250px;
+  height: 250px;
+  margin-top: -125px;
+  margin-left: -125px;
+
+  border-width: 1.8px;
+  border-style: dashed;
+  border-color: white;
+  border-radius: 50%;
+
+  -webkit-animation: spin-left 10s linear infinite;
+  -moz-animation: spin-left 10s linear infinite;
+  -ms-animation: spin-left 10s linear infinite;
+  -o-animation: spin-left 10s linear infinite;
+  animation: spin-left 10s linear infinite;
+}
+
+
+#Planet1-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -20px;
+  margin-top: -26px;
+  color: white;
+  font-size: 11px;
+
   -webkit-animation: spin-right 10s linear infinite;
+  -moz-animation: spin-right 10s linear infinite;
+  -ms-animation: spin-right 10s linear infinite;
+  -o-animation: spin-right 10s linear infinite;
   animation: spin-right 10s linear infinite;
 }
 
-/* ---------- Repeating styles for the inner orbiting circles ---------- */
-.inner-orbit-cirlces {
-  position: absolute;
-  top: 62px;
-  left: -6px;
-  height: 10px;
-  width: 10px;
-  border-radius: 100%;
-  background-color: #9F98E6;
-}
+/*Planet 2 Group*/
 
-/* ---------- Middle orbit - This is the circles second closest to the central point ---------- */
-#middle-orbit {
+#Planet2 {
   position: absolute;
-  top: 35px;
-  left: 35px;
-  width: 225px;
-  height: 225px;
-  border: 2px #4A437F dashed;
-  border-radius: 100%;
-  -webkit-animation: spin-right 15s linear infinite;
-  animation: spin-right 15s linear infinite;
-}
-
-/* ---------- Repeating styles for the inner orbiting circles ---------- */
-.middle-orbit-cirlces {
-  position: absolute;
-  top: 25px;
-  left: 17px;
-  height: 20px;
+  top:  0;
+  left: 50%;
   width: 20px;
-  border-radius: 100%;
-  background-color: #4A437F;
+  height: 20px;
+  margin-left: -10px;
+  margin-top: -13px;
+  background: white;
+  border-color: white;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 50%; 
 }
 
-/* ---------- Outer orbit - This is the circles furthest away from the central point ---------- */
-#outer-orbit {
+#Planet2-orbit {
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 294px;
-  height: 294px;
-  border: 2px #4A437F dashed;
-  border-radius: 100%;
+  top: 50%;
+  left: 50%;
+
+  width: 500px;
+  height: 500px;
+  margin-top: -250px;
+  margin-left: -250px;
+
+  border-width: 2.2px;
+  border-style: dashed;
+  border-color: white;
+  border-radius: 80%;
+
+  -webkit-animation: spin-right 100s linear infinite;
+  -moz-animation: spin-right 100s linear infinite;
+  -ms-animation: spin-right 100s linear infinite;
+  -o-animation: spin-right 100s linear infinite;
+  animation: spin-right 100s linear infinite;
+}
+
+#Planet2-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -20px;
+  margin-top: -25px;
+  color: white;
+  font-size: 11px;
+
+  -webkit-animation: spin-left 100s linear infinite;
+  -moz-animation: spin-left 100s linear infinite;
+  -ms-animation: spin-left 100s linear infinite;
+  -o-animation: spin-left 100s linear infinite;
+  animation: spin-left 100s linear infinite;
+}
+
+/*Planet 3 Group*/
+
+#Planet3 {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 50px;
+  height: 50px;
+  margin-left: -25px;
+  margin-top: -28px;
+  background: white;
+  border-color: white;
+  border-width: 2px;
+  border-style: solid;
+  border-radius: 50%;     
+}
+
+#Planet3-1 {
+  width: 8px;
+  height: 8px;
+  margin-left: 10px;
+  margin-top: 10px;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 64px white; 
+}
+
+#Planet3-2 {
+  width: 10px;
+  height: 10px;
+  margin-left: 23px;
+  margin-top: 16px;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 64px white; 
+}
+
+#Planet3-3 {
+  width: 12px;
+  height: 12px;
+  margin-left: 30px;
+  margin-top: 30px;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 64px white; 
+}
+
+#Planet3-orbit {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  width: 800px;
+  height: 800px;
+  margin-top: -400px;
+  margin-left: -400px;
+
+  border-width:3px;
+  border-style: dashed;
+  border-color: white;
+  border-radius: 50%;
+
+  -webkit-animation: spin-right 200s linear infinite;
+  -moz-animation: spin-right 200s linear infinite;
+  -ms-animation: spin-right 200s linear infinite;
+  -o-animation: spin-right 200s linear infinite;
+  animation: spin-right 200s linear infinite;
+}
+
+#Planet3-1-orbit {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 100px;
+  height: 100px;
+  margin-left: -50px;
+  margin-top: -52px;
+
+  border: 1px dashed white;
+  border-radius: 50%;
+
+  -webkit-animation: spin-left 5s linear infinite;
+  -moz-animation: spin-left 5s linear infinite;
+  -ms-animation: spin-left 5s linear infinite;
+  -o-animation: spin-left 5s linear infinite;
+  animation: spin-left 5s linear infinite;
+}
+
+#Planet3-2-orbit {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 170px;
+  height: 170px;
+  margin-left: -85px;
+  margin-top: -85px;
+
+  border: 1px dashed white;
+  border-radius: 50%;
+
   -webkit-animation: spin-right 20s linear infinite;
+  -moz-animation: spin-right 20s linear infinite;
+  -ms-animation: spin-right 20s linear infinite;
+  -o-animation: spin-right 20s linear infinite;
   animation: spin-right 20s linear infinite;
 }
 
-/* ---------- Repeating styles for the outer orbiting circles ---------- */
-.outer-orbit-cirlces {
+#Planet3-3-orbit {
   position: absolute;
-  top: -17px;
-  left: 125px;
-  height: 30px;
-  width: 30px;
-  border-radius: 100%;
-  background-color: #00FFCA;
+      top:  0;
+      left: 50%;
+      width: 250px;
+      height: 250px;
+    margin-left: -125px;
+      margin-top: -125px;
+    
+    border: 1px dashed white;
+    border-radius: 50%;
+    
+    -webkit-animation: spin-left 30s linear infinite;
+    -moz-animation: spin-left 30s linear infinite;
+    -ms-animation: spin-left 30s linear infinite;
+    -o-animation: spin-left 30s linear i0snfinite;
+    animation: spin-left 30s linear infinite;
 }
 
-/* ---------- Animation ---------- */
+#Planet3-label {  
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -29px;
+  margin-top: -32px;
+  color: white;
+  font-size: 11px;
+
+  -webkit-animation: spin-left 200s linear infinite;
+  -moz-animation: spin-left 200s linear infinite;
+  -ms-animation: spin-left 200s linear infinite;
+  -o-animation: spin-left 200s linear infinite;
+  animation: spin-left 200s linear infinite; 
+}
+
+/*The Animation time for moon labels is calculated by using harmonic sums. See Readme.md*/
+
+#Planet3-1-label {  
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -54px;
+  margin-top: -2px;
+  color: white;
+  font-size: 11px;
+
+  -webkit-animation: spin-right 5.12820512820512820512820s linear infinite;
+  -moz-animation: spin-right 5.12820512820512820512820s linear infinite;
+  -ms-animation: spin-right 5.12820512820512820512820s linear infinite;
+  -o-animation: spin-right 5.12820512820512820512820s linear infinite;
+  animation: spin-right 5.12820512820512820512820s linear infinite;
+}
+
+#Planet3-2-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -78px;
+  margin-top: 2px;
+  color: white;
+  font-size: 11px;
+
+  -webkit-animation: spin-left 18.1818181818181818s linear infinite;
+  -moz-animation: spin-left 18.1818181818181818s linear infinite;
+  -ms-animation: spin-left 18.1818181818181818s linear infinite;
+  -o-animation: spin-left 18.1818181818181818s linear infinite;
+  animation: spin-left 18.1818181818181818s linear infinite;
+}
+
+#Planet3-3-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -110px;
+  margin-top: 14px;
+  color: white;
+  font-size: 11px;
+
+  -webkit-animation: spin-right 35.29411764705882352s linear infinite;
+  -moz-animation: spin-right 35.29411764705882352s linear infinite;
+  -ms-animation: spin-right 35.29411764705882352s linear infinite;
+  -o-animation: spin-right 35.29411764705882352s linear infinite;
+  animation: spin-right 35.29411764705882352s linear infinite;
+}
+
+/*Planet 4 Group*/
+
+#Planet4 {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 30px;
+  height: 30px;
+  margin-left: -15px;
+  margin-top: -18px;
+  background: white;
+  border-color: white;
+  border-width: 2px;
+  border-style: solid;
+  border-radius: 50%;     
+}
+
+#Planet4-1 {
+  width: 10px;
+  height: 10px;
+  margin-left: 10px;
+  margin-top: 10px;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 64px white; 
+}
+
+#Planet4-orbit {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  
+  width: 1200px;
+  height: 1200px;
+  margin-top: -600px;
+  margin-left: -600px;
+  
+  border-width:4px;
+  border-style: dashed;
+  border-color: white;
+  border-radius: 50%;
+  
+  -webkit-animation: spin-right 150s linear infinite;
+  -moz-animation: spin-right 150s linear infinite;
+  -ms-animation: spin-right 150s linear infinite;
+  -o-animation: spin-right 150s linear infinite;
+  animation: spin-right 150s linear infinite;
+  position: absolute;  
+}
+
+#Planet4-1-orbit {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 100px;
+  height: 100px;
+  margin-left: -50px;
+  margin-top: -50px;
+
+  border: 1px dashed white;
+  border-radius: 50%;
+
+  -webkit-animation: spin-right 30s linear infinite;
+  -moz-animation: spin-right 30s linear infinite;
+  -ms-animation: spin-right 30s linear infinite;
+  -o-animation: spin-right 30s linear i0snfinite;
+  animation: spin-right 30s linear infinite;
+}
+
+#Planet4-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -25px;
+  margin-top: -25px;
+  color: white;
+  font-size: 11px;
+    
+  -webkit-animation: spin-left 150s linear infinite;
+  -moz-animation: spin-left 150s linear infinite;
+  -ms-animation: spin-left 150s linear infinite;
+  -o-animation: spin-left 150s linear infinite;
+  animation: spin-left 150s linear infinite;
+}
+
+#Planet4-1-label {
+  position: absolute;
+  top:  0;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  margin-left: -58px;
+  margin-top: 0px;
+  color: white;
+  font-size: 11px;
+    
+  -webkit-animation: spin-left 25s linear infinite;
+  -moz-animation: spin-left 25s linear infinite;
+  -ms-animation: spin-left 25s linear infinite;
+  -o-animation: spin-left 25s linear infinite;
+  animation: spin-left 25s linear infinite; 
+}
+
+/*Defining the rotational keyframe at-rules*/
+
 @-webkit-keyframes spin-right {
   100% {
     -webkit-transform: rotate(360deg);
-    -moz-transform: rotate(360deg);
-    -ms-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
-    transform: rotate(360deg);
+       -moz-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+         -o-transform: rotate(360deg);
+            transform: rotate(360deg);
+  }
+}
+
+@keyframes spin-right {
+  100% {
+    -webkit-transform: rotate(360deg);
+       -moz-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+         -o-transform: rotate(360deg);
+            transform: rotate(360deg);
+  }
+}
+
+@-webkit-keyframes spin-left {
+  100% {
+    -webkit-transform: rotate(-360deg);
+       -moz-transform: rotate(-360deg);
+        -ms-transform: rotate(-360deg);
+         -o-transform: rotate(-360deg);
+            transform: rotate(-360deg);
+  }
+}
+
+@keyframes spin-left {
+  100% {
+    -webkit-transform: rotate(-360deg);
+       -moz-transform: rotate(-360deg);
+        -ms-transform: rotate(-360deg);
+         -o-transform: rotate(-360deg);
+            transform: rotate(-360deg);
   }
 }
 
